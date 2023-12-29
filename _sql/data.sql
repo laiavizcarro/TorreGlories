f6cc4f06a2621d62ced5e807db372026e057e01d
laiavizcarro <145133090+laiavizcarro@users.noreply.github.com>
2023-12-29 18:52:12 +0100

    Database scripts + Allergens images

--- _sql/data.sql
+++ _sql/data.sql
@@ -1,17 +1,83 @@
-CREATE DATABASE IF NOT EXISTS torreglories;
-
 USE torreglories;
 
-DROP TABLE order_products;
-DROP TABLE orders;
-DROP TABLE product_allergens;
-DROP TABLE products;
-DROP TABLE categories;
-DROP TABLE allergens;
-DROP TABLE roles;
-DROP TABLE users;
+--
+-- Bolcament de dades per a la taula 'allergens'
+--
+INSERT INTO allergens (code, name, description, img_path) VALUES
+('AC', 'Crustacis', 'Crustacis i els seus derivats', '/images/allergens/AC.png'),
+('AE', 'Ous', 'Ous i els seus derivats', '/images/allergens/AE.png'),
+('AF', 'Peix', 'Peix i els seus derivats', '/images/allergens/AF.png'),
+('AM', 'Lactis', 'Llet i els seus derivats', '/images/allergens/AM.png'),
+('AN', 'Fruits de closca', 'Fruits secs i els seus derivats', '/images/allergens/AN.png'),
+('AP', 'Cacauets', 'Cacauets i els seus derivats', '/images/allergens/AP.png'),
+('AS', 'Sèsam', 'Llavors de sèsam i els seus derivats', '/images/allergens/AS.png'),
+('AU', 'Sulfits', 'Diòxid de sofre i sulfits en el producte', '/images/allergens/AU.png'),
+('AW', 'Gluten', 'Cereals amb gluten i els seus derivats', '/images/allergens/AW.png'),
+('AY', 'Soja', 'Soja i els seus derivats', '/images/allergens/AY.png'),
+('BC', 'Api', 'Api i els seus derivats', '/images/allergens/BC.png'),
+('BM', 'Mostassa', 'Mostassa i els seus derivats', '/images/allergens/BM.png'),
+('NL', 'Tramussos', 'Tramussos o lupino i els seus derivats', '/images/allergens/NL.png'),
+('UM', 'Mol·luscs', 'Mol·luscs i els seus derivats', '/images/allergens/UM.png');
 
-CREATE TABLE roles (
-    "id" NOT NULL TINYINT(1),
-    "name" NOT NULL VARCHAR2(50)
-)
+--
+-- Bolcament de dades per a la taula 'categories'
+--
+INSERT INTO categories (name) VALUES
+('menu'),
+('dessert'),
+('drinkCoffee'),
+('breakfastSnack');
+
+--
+-- Bolcament de dades per a la taula 'products'
+--
+INSERT INTO products (name, category_id, stock, iva, base_price, total_price, is_offer, offer_price, total_offer_price, img_path) VALUES
+('Canelons de rostit', 1, 50, 4, 10.00, 12.00, 0, 0.00, 0.00, 'images/uploads/canelons.jpg'),
+("Canelons d\'espinacs i pinyons", 1, 50, 4, 8.00, 10.00, 0, 0.00, 0.00, 'images/uploads/canelonsespinacs.jpg'),
+('Amanida variada amb panses', 1, 50, 4, 10.00, 15.00, 0, 0.00, 0.00, 'images/uploads/amanida.jpg'),
+('Spaghetti a la bolonyesa', 1, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/spagettis.jpg'),
+('Macedonia', 2, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/macedonia.jpg'),
+('Pastís de formatge', 2, 50, 4, 4.13, 5.00, 0, 0.00, 0.00, 'images/uploads/pastis-formatge.webp'),
+('Iogurt natural', 2, 50, 4, 2.64, 3.20, 0, 0.00, 0.00, 'images/uploads/iogurt.jpg'),
+('CocaCola', 3, 50, 4, 1.10, 1.50, 0, 0.00, 0.00, 'images/uploads/cocacola.jpg'),
+('Arros del senyoret', 1, 50, 4, 25.00, 30.25, 0, 0.00, 0.00, 'images/uploads/arros-senyoret.jpeg'),
+('Hamburguesa de vedella acompanyada de patates', 1, 50, 4, 10.00, 12.00, 0, 0.00, 0.00, 'images/uploads/hamburguesa.jpeg'),
+('Saltejat de verdures i xampinyons', 1, 50, 4, 8.00, 10.00, 0, 0.00, 0.00, 'images/uploads/saltejat-verdures.jpg'),
+('Rotllets vietnamites', 1, 50, 4, 10.00, 15.00, 0, 0.00, 0.00, 'images/uploads/rollitos.webp'),
+('Xai al forn', 1, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/xai.jpg'),
+('Fingers de pollastre i patates', 1, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/fingers.jpg'),
+('Sushi de salmó i tonyina', 1, 50, 4, 4.13, 5.00, 0, 0.00, 0.00, 'images/uploads/nigiris.jpg'),
+('Dorada al forn i acompanyament', 1, 50, 4, 2.64, 3.20, 0, 0.00, 0.00, 'images/uploads/daurada.jpg'),
+('Dues boles de gelat', 2, 50, 4, 1.10, 1.50, 0, 0.00, 0.00, 'images/uploads/gelat.webp'),
+('Flam dou', 2, 50, 4, 10.00, 12.00, 0, 0.00, 0.00, 'images/uploads/flam.webp'),
+('Crema catalana', 2, 50, 4, 8.00, 10.00, 0, 0.00, 0.00, 'images/uploads/crema-catalana.jpg'),
+('Pastís de Santiago', 2, 50, 4, 10.00, 15.00, 0, 0.00, 0.00, 'images/uploads/pastis-santiago.jpg'),
+('Milfulls de crema', 2, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/milfulls.jpg'),
+('Macarons', 2, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/macaron.jpg'),
+('Aigua 1L', 3, 50, 4, 4.13, 5.00, 0, 0.00, 0.00, 'images/uploads/aigua.jpg'),
+('Coca Cola Zero', 3, 50, 4, 2.64, 3.20, 0, 0.00, 0.00, 'images/uploads/cocacolazero.jpeg'),
+('Fanta taronja', 3, 50, 4, 1.10, 1.50, 0, 0.00, 0.00, 'images/uploads/fanta-taronja.jpg');
+
+--
+-- Bolcament de dades per a la taula 'product_allergens'
+--
+INSERT INTO product_allergens (allergen_id, product_id) VALUES
+(4, 1),
+(9, 1),
+(4, 2),
+(9, 2),
+(3, 15);
+
+--
+-- Bolcament de dades per a la taula 'roles'
+--
+INSERT INTO roles (name) VALUES
+('admin'),
+('basic');
+
+--
+-- Bolcament de dades per a la taula 'users'
+--
+INSERT INTO users (name, surname, email, phone_number, password, role_id, incorporation_date) VALUES
+('Admin', 'Admin', 'admin@gmail.com', '123456789', '21232f297a57a5a743894a0e4a801fc3', 1, NULL), -- password: admin
+('Laia', 'Vizcarro', 'vizcarrolaia@gmail.com', '671704007', '81dc9bdb52d04dc20036dbd8313ed055', 2, NULL); -- password: 1234
--- _sql/structure.sql
+++ _sql/structure.sql
@@ -1,47 +1,155 @@
-CREATE TABLE "products" (
-  "id" int(50) NOT NULL,
-  "name" varchar(50) NOT NULL,
-  "category_id" int(50) NOT NULL,
-  "stock" int(100) NOT NULL,
-  "iva" int(2) NOT NULL,
-  "base_price" decimal(5,2) NOT NULL,
-  "total_price" decimal(5,2) NOT NULL,
-  "is_offer" tinyint(1) NOT NULL,
-  "offer_price" decimal(5,2) NOT NULL,
-  "total_offer_price" decimal(5,2) NOT NULL,
-  "img_path" varchar(255) DEFAULT NULL
-) /*ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;*/
+--
+-- Base de dades: `torreglories`
+--
 
-INSERT INTO products ('id', 'name', 'category_id', 'stock', 'iva', 'base_price', 'total_price', 'is_offer', 'offer_price', 'total_offer_price', 'img_path') VALUES
-(1, 'Canelons de rostit', 1, 50, 4, 10.00, 12.00, 0, 0.00, 0.00, 'images/uploads/canelons.jpg'),
-(2, 'Canelons despinacs i pinyons', 1, 50, 4, 8.00, 10.00, 0, 0.00, 0.00, 'images/uploads/canelonsespinacs.jpg'),
-(3, 'Amanida variada amb panses', 1, 50, 4, 10.00, 15.00, 0, 0.00, 0.00, 'images/uploads/amanida.jpg'),
-(4, 'Spaghetti a la bolonyesa', 1, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/spagettis.jpg'),
-(5, 'Macedonia', 2, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/macedonia.jpg'),
-(6, 'Pastís de formatge', 2, 50, 4, 4.13, 5.00, 0, 0.00, 0.00, 'images/uploads/pastis-formatge.webp'),
-(7, 'Iogurt natural', 2, 50, 4, 2.64, 3.20, 0, 0.00, 0.00, 'images/uploads/iogurt.jpg'),
-(8, 'CocaCola', 3, 50, 4, 1.10, 1.50, 0, 0.00, 0.00, 'images/uploads/cocacola.jpg'),
-(9, 'Arros del senyoret', 1, 50, 4, 25.00, 30.25, 0, 0.00, 0.00, 'images/uploads/arros-senyoret.jpeg'),
-(10, 'Hamburguesa de vedella acompanyada de patates', 1, 50, 4, 10.00, 12.00, 0, 0.00, 0.00, 'images/uploads/hamburguesa.jpeg'),
-(11, 'Saltejat de verdures i xampinyons', 1, 50, 4, 8.00, 10.00, 0, 0.00, 0.00, 'images/uploads/saltejat-verdures.jpg'),
-(12, 'Rotllets vietnamites', 1, 50, 4, 10.00, 15.00, 0, 0.00, 0.00, 'images/uploads/rollitos.webp'),
-(13, 'Xai al forn', 1, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/xai.jpg'),
-(14, 'Fingers de pollastre i patates', 1, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/fingers.jpg'),
-(15, 'Sushi de salmó i tonyina', 1, 50, 4, 4.13, 5.00, 0, 0.00, 0.00, 'images/uploads/nigiris.jpg'),
-(16, 'Dorada al forn i acompanyament', 1, 50, 4, 2.64, 3.20, 0, 0.00, 0.00, 'images/uploads/daurada.jpg'),
-(17, 'Dues boles de gelat', 2, 50, 4, 1.10, 1.50, 0, 0.00, 0.00, 'images/uploads/gelat.webp'),
-(18, 'Flam dou', 2, 50, 4, 10.00, 12.00, 0, 0.00, 0.00, 'images/uploads/flam.webp'),
-(19, 'Crema catalana', 2, 50, 4, 8.00, 10.00, 0, 0.00, 0.00, 'images/uploads/crema-catalana.jpg'),
-(20, 'Pastís de Santiago', 2, 50, 4, 10.00, 15.00, 0, 0.00, 0.00, 'images/uploads/pastis-santiago.jpg'),
-(21, 'Milfulls de crema', 2, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/milfulls.jpg'),
-(22, 'Macarons', 2, 50, 4, 10.00, 12.10, 0, 0.00, 0.00, 'images/uploads/macaron.jpg'),
-(23, 'Aigua 1L', 3, 50, 4, 4.13, 5.00, 0, 0.00, 0.00, 'images/uploads/aigua.jpg'),
-(24, 'Coca Cola Zero', 3, 50, 4, 2.64, 3.20, 0, 0.00, 0.00, 'images/uploads/cocacolazero.jpeg'),
-(25, 'Fanta taronja', 3, 50, 4, 1.10, 1.50, 0, 0.00, 0.00, 'images/uploads/fanta-taronja.jpg');
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
-insert into allergens ('id', 'code', 'name', 'description', 'img_path') VALUES 
-();
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS torreglories;   
+USE torreglories;
+
+START TRANSACTION;
+
+DROP TABLE IF EXISTS order_products;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS product_allergens;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS allergens;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+
+--
+-- Estructura de la taula `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` tinyint(1) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) NOT NULL,
+
+  CONSTRAINT PK_ROLES PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de la taula `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  `password` varchar(50) NOT NULL,
+  `role_id` tinyint(1) NOT NULL,
+  `incorporation_date` date DEFAULT NULL,
+
+  CONSTRAINT PK_USERS PRIMARY KEY (id),
+  CONSTRAINT FK_USERS_ROLE_ID FOREIGN KEY (role_id) REFERENCES roles(id),
+  CONSTRAINT UK_USERS_EMAIL UNIQUE KEY (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de la taula `allergens`
+--
+
+CREATE TABLE `allergens` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `img_path` varchar(255) DEFAULT NULL,
+
+  CONSTRAINT PK_ALLERGENS PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de la taula `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+
+  CONSTRAINT PK_CATEGORIES PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de la taula `products`
+--
+
+CREATE TABLE `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `category_id` int(50) NOT NULL,
+  `stock` int(100) NOT NULL,
+  `iva` int(2) NOT NULL,
+  `base_price` decimal(5,2) NOT NULL,
+  `total_price` decimal(5,2) NOT NULL,
+  `is_offer` tinyint(1) NOT NULL,
+  `offer_price` decimal(5,2) NOT NULL,
+  `total_offer_price` decimal(5,2) NOT NULL,
+  `img_path` varchar(255) DEFAULT NULL,
+
+  CONSTRAINT PK_PRODUCTS PRIMARY KEY (id),
+  CONSTRAINT FK_PRODUCTS_CATEGORY_ID FOREIGN KEY (category_id) REFERENCES categories (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de la taula `product_allergens`
+--
+
+CREATE TABLE `product_allergens` (
+  `allergen_id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+
+  CONSTRAINT PK_PRODUCT_ALLERGENS PRIMARY KEY (allergen_id, product_id),
+  CONSTRAINT FK_PRODUCT_ALLERGENS_ALLERGEN_ID FOREIGN KEY (allergen_id) REFERENCES allergens (id),
+  CONSTRAINT FK_PRODUCT_ALLERGENS_PRODUCT_ID FOREIGN KEY (product_id) REFERENCES products (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-/* roles */
-INSERT INTO `roles` (`id`, `name`) VALUES (NULL, 'admin'), (NULL, 'basic');
+--
+-- Estructura de la taula `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `date` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `is_paid` tinyint(1) NOT NULL,
+  
+  CONSTRAINT PK_ORDERS PRIMARY KEY (id),
+  CONSTRAINT FK_ORDERS_USER_ID FOREIGN KEY (user_id) REFERENCES users (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de la taula `order_products`
+--
+
+CREATE TABLE `order_products` (
+  `order_id` int NOT NULL,
+  `product_id` int NOT NULL,
+   `name` varchar(50) NOT NULL,
+  `quantity` int(5) NOT NULL,
+  `iva` int(2) NOT NULL,
+  `base_price` decimal(5,2) NOT NULL,
+  `total_price` decimal(5,2) NOT NULL,
+  `is_offer` tinyint(1) NOT NULL,
+  `offer_price` decimal(5,2) NOT NULL,
+  `total_offer_price` decimal(5,2) NOT NULL,
+
+  CONSTRAINT PK_ORDER_PRODUCTS PRIMARY KEY (order_id, product_id),
+  CONSTRAINT FK_ORDER_PRODUCTS_PRODUCT_ID FOREIGN KEY (product_id) REFERENCES products (id),
+  CONSTRAINT FK_ORDER_PRODUCTS_ORDER_ID FOREIGN KEY (order_id) REFERENCES orders (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
