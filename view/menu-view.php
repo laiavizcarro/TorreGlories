8c9c0b4e28ea4728951a8afd70053a7d049334ab
laiavizcarro <145133090+laiavizcarro@users.noreply.github.com>
2024-01-26 16:09:46 +0100

    Add categories checkbox

--- view/menu-view.php
+++ view/menu-view.php
@@ -1,35 +1,42 @@
 <section class="container text-center mt-70">
-	<div>
-		<h2>Consulta els productes</h2>
-		<h4>Consulta els plats a escollir de la carta</h4>
-	</div>
+    <div>
+        <h2>Consulta els productes</h2>
+        <h4>Consulta els plats a escollir de la carta</h4>
+    </div>
 </section>
 
 <section>
-	<div class=""> 
-		<ul class="nav container justify-content-end">
-			<li class="nav-item">
-				<a class="nav-link btn-category <?= str_contains($_SERVER['REQUEST_URI'], 'category_id=1') ? 'btn-category_active' : '' ?>" 
-					href="<?= url ?>/index.php?controller=Product&action=products&category_id=1">Plats
+    <div class="">
+        <ul class="nav container justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link btn-category <?= str_contains($_SERVER['REQUEST_URI'], 'category_id=1') ? 'btn-category_active' : '' ?>"
+                href="<?= url ?>/index.php?controller=Product&action=products&category_id=1">Plats
                 </a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link btn-category <?= str_contains($_SERVER['REQUEST_URI'], 'category_id=2') ? 'btn-category_active' : '' ?>" 
-					href="<?= url ?>/index.php?controller=Product&action=products&category_id=2">Postres
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn-category <?= str_contains($_SERVER['REQUEST_URI'], 'category_id=2') ? 'btn-category_active' : '' ?>"
+                href="<?= url ?>/index.php?controller=Product&action=products&category_id=2">Postres
                 </a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link btn-category <?= str_contains($_SERVER['REQUEST_URI'], 'category_id=3') ? 'btn-category_active' : '' ?>" 
-					href="<?= url ?>/index.php?controller=Product&action=products&category_id=3">Begudes
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn-category <?= str_contains($_SERVER['REQUEST_URI'], 'category_id=3') ? 'btn-category_active' : '' ?>"
+                href="<?= url ?>/index.php?controller=Product&action=products&category_id=3">Begudes
                 </a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link btn-category <?= str_contains($_SERVER['REQUEST_URI'], 'category_id=4') ? 'btn-category_active' : '' ?>" 
-					href="<?= url ?>/index.php?controller=Product&action=products&category_id=4">Esmorzars
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn-category <?= str_contains($_SERVER['REQUEST_URI'], 'category_id=4') ? 'btn-category_active' : '' ?>"
+                href="<?= url ?>/index.php?controller=Product&action=products&category_id=4">Esmorzars
                 </a>
-			</li>
-		</ul>
-	</div>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<section class="nav container justify-content-end">
+    <div><input type="checkbox" name="plats" value="plats" id="plats"><label for="plats">Plats</label></div>
+    <div><input type="checkbox" name="postres" value="postres" id="postres"><label for="postres">Postres</label></div>
+    <div><input type="checkbox" name="begudes" value="begudes" id="begudes"><label for="begudes">Begudes</label></div>
+    <div><input type="checkbox" name="esmorzars" value="esmorzars" id="esmorzars"><label for="esmorzars">Esmorzars</label></div>
 </section>
 
 <section class="container mt-70">
@@ -39,8 +46,8 @@
                 <div class="card product-card">
                     <div class="row g-0">
                         <div class="col-xs-4 col-sm-4 col-md-4 card-image">
-                            <img src="<?php echo $product->getImgPath() ?>" 
-                                alt="Imatge del producte: <?= $product->getName() ?>">
+                            <img src="<?php echo $product->getImgPath() ?>"
+                            alt="Imatge del producte: <?= $product->getName() ?>">
                         </div>
 
                         <div class="col-xs-8 col-sm-8 col-md-8">
